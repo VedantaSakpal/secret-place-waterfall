a23a902b2e8044b9852a4a606d5b9bbde0836f36
Vedant Sakpal <vsakpal541@@gmail.com>
2026-06-11 23:10:43 +0530

    adding new images

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#F8FAF9',
+          500: '#84A98C',
+          900: '#065F46'
+        }
+      }
+    }
+  },
+  plugins: []
+}
